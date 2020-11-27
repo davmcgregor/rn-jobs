b6a16dfdfb87dbf205464a83ee047791833fe672
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-27 18:41:38 +0000

    Create screens boilerplate

---
+++ screens/AuthScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const AuthScreen = () => {
+  return (
+    <View>
+      <Text>
+        AuthScreen
+      </Text>
+      <Text>
+        AuthScreen
+      </Text>
+      <Text>
+        AuthScreen
+      </Text>
+      <Text>
+        AuthScreen
+      </Text>
+      <Text>
+        AuthScreen
+      </Text>
+      <Text>
+        AuthScreen
+      </Text>
+    </View>
+  )
+}
+
+export default AuthScreen

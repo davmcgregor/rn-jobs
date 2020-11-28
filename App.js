8cdff1d694eb8da0f4d9c9802f202ec2c92c182c
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-28 20:51:24 +0000

    Set up Redux

--- App.js
+++ App.js
@@ -4,31 +4,35 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import AuthScreen from './src/screens/AuthScreen'
-import WelcomeScreen from './src/screens/WelcomeScreen'
-import MapScreen from './src/screens/MapScreen'
-import DeckScreen from './src/screens/DeckScreen'
-import SettingsScreen from './src/screens/SettingsScreen'
-import ReviewScreen from './src/screens/ReviewScreen'
+import { Provider } from 'react-redux';
+import store from './store';
+
+import AuthScreen from './src/screens/AuthScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
 
 const MainNavigator = createBottomTabNavigator({
-  welcome: WelcomeScreen, 
-  auth: AuthScreen ,
+  welcome: WelcomeScreen,
+  auth: AuthScreen,
   main: createBottomTabNavigator({
     map: MapScreen,
     deck: DeckScreen,
     review: createStackNavigator({
       review: ReviewScreen,
-      settings: SettingsScreen
-    })
-  })
-})
+      settings: SettingsScreen,
+    }),
+  }),
+});
 
 const App = createAppContainer(MainNavigator);
 
 export default () => {
   return (
-    <App /> 
+    <Provider store={store}>
+      <App />
+    </Provider>
   );
-}
-
+};

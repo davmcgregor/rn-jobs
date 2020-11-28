fe0954b0f8aea91f5daa9079b0571f82c49eeab7
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-28 15:18:00 +0000

    Style WelcomeScreen

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native';
 
-const Slides = () => {
-  return <View></View>;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const Slides = ({ data }) => {
+  const renderSlides = () => {
+    return data.map((slide) => {
+      return (
+        <View
+          key={slide.text}
+          style={[styles.slideStyle, { backgroundColor: slide.color }]}
+        >
+          <Text style={styles.slideText}>{slide.text}</Text>
+        </View>
+      );
+    });
+  };
+
+  return (
+    <ScrollView horizontal pagingEnabled style={{ flex: 1 }}>
+      {renderSlides()}
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  slideStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+  },
+  slideText: {
+    fontSize: 30,
+    color: 'white',
+  },
+});
 
 export default Slides;

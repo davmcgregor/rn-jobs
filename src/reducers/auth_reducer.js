d1109907401bd3daf74c3e72ee104a990f7496d1
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-29 16:32:07 +0000

    Set up Auth Reducer

---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,18 @@
+import {
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_SUCCESS_FAIL,
+} from '../actions/types';
+
+const initialState = {};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case FACEBOOK_LOGIN_SUCCESS:
+      return { token: payload };
+    case FACEBOOK_LOGIN_SUCCESS_FAIL:
+      return { token: null };
+    default:
+      return state;
+  }
+}

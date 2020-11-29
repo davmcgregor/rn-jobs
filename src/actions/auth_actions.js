6b9baa78e4e787f1d5f880bdc329b700f7ce20c1
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-29 13:00:04 +0000

    Create doFacebookLogin helper

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,15 +1,36 @@
+import { FB_APP_ID } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Facebook from 'expo-facebook';
 
-import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from './types';
 
-export const facebookLogin = () => async dispatch => {
+export const facebookLogin = () => async (dispatch) => {
   let token = await AsyncStorage.getItem('fb_token');
 
   if (token) {
-    // dispatch({ type: 'signin', payload: token });
-    // navigate('TrackList');
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
-    // navigate('Signup');
+    doFacebookLogin(dispatch);
   }
+};
 
-}
+const doFacebookLogin = async (dispatch) => {
+  try {
+    await Facebook.initializeAsync({
+      appId: FB_APP_ID,
+    });
+
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+      permissions: ['public_profile'],
+    });
+
+    if (type === 'cancel') {
+      return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+    }
+
+    await AsyncStorage.setItem('fb_token', token);
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } catch ({ message }) {
+    alert(`Facebook Login Error: ${message}`);
+  }
+};
